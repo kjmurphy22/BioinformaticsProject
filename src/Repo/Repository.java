801968c96b53c24fef8917d7895173b9d9c9c34a
kjmurphy22 <99885084+kjmurphy22@users.noreply.github.com>
2023-01-06 16:43:42 +0100

    added Repository and TechnicalSupport comments

--- src/Repo/Repository.java
+++ src/Repo/Repository.java
@@ -6,12 +6,30 @@ import Fasta.StandardAlignment;
 
 import java.util.HashMap;
 
+/*
+Repository is a class that stores Alignment objects. It has three private variables.
+optimalAlignment is an OptimalAlignment object
+SNPAlignment is a SNPAlignment object.
+userAlignments is a HashMap<String, StandardAlignment> object storing the names of bioinformaticians who have
+been assigned an alignment as keys and their respective StandardAlignment object as values.
+*/
+
 public class Repository {
 
     private OptimalAlignment optimalAlignment;
     private SNPAlignment SNPAlignment;
     private HashMap<String, StandardAlignment> userAlignments;
 
+    /*
+    Repository objects only have constructor and getter and setter methods.
+    */
+
+    /*
+    Repository has two constructors. This constructor creates a new repository from an OptimalAlignment object and
+    a HashMap<String, StandardAlignment> object. It's called in the main method to create the initial repository.
+    It is also called in the Repository(repository) constructor to create new Repository objects from other
+    Repository objects.
+    */
     public Repository(OptimalAlignment optimalAlignment, HashMap<String, StandardAlignment> userAlignments){
         System.out.println("Creating repository...\n");
         OptimalAlignment optimal = new OptimalAlignment(optimalAlignment.getGenomes(), optimalAlignment.getReferenceGenome());
@@ -30,6 +48,12 @@ public class Repository {
         this.userAlignments = standardAlignments;
         System.out.println("Repository created.\n");
     }
+
+    /*
+    Repository has two constructors. This constructor creates a new Repository object from an existing Repository
+    object. It is used by TechnicalSupport objects to create backups from current repositories and restore
+    repositories from backup repositories.
+    */
 
     public Repository(Repository repository) {
         Repository repo = new Repository(repository.getOptimalAlignment(), repository.getUserAlignments());
--- src/Team/TechnicalSupport.java
+++ src/Team/TechnicalSupport.java
@@ -10,6 +10,9 @@ import java.util.HashMap;
 
 /*
 TechnicalSupport is a concrete class that extends TeamMember and implements the Supportable interface.
+It has one private static variable and one private instance variable.
+numberOfTechnicalSupports counts the number of TechnicalSupport objects created after TeamReader object calls createTeam in MainMethod.
+lastBackupDateTime is a String that stores the date and time of the last backup of the repository.
 */
 
 public class TechnicalSupport extends TeamMember implements Supportable {
@@ -20,8 +23,9 @@ public class TechnicalSupport extends TeamMember implements Supportable {
         numberOfTechnicalSupports++;
     }
 
-    protected String lastBackupDateTime = "No existing repository backup.\n";
+    private String lastBackupDateTime = "No existing repository backup.\n";
 
+    /* TechnicalSupport only has one constructor and uses the super constructor from TeamMember */
     public TechnicalSupport(String name, int years) {
         super(RoleType.TECHNICALSUPPORT, name, years);
     }
@@ -38,33 +42,58 @@ public class TechnicalSupport extends TeamMember implements Supportable {
         return lastBackupDateTime;
     }
 
-    public HashMap<String, StandardAlignment> getAllUserAlignments(Repository repository){
+    /* getRepositoryUserStandardAlignment retrieves the StandardAlignment of a specific user in the repository */
+    public StandardAlignment getRepositoryUserStandardAlignment(String name, Repository repository){
+        return repository.getUserAlignments().get(name);
+    }
+
+    /* getAllRepositoryUserStandardAlignments retrieves all StandardAlignments in the repository */
+    public HashMap<String, StandardAlignment> getAllRepositoryUserStandardAlignments(Repository repository){
         return repository.getUserAlignments();
     }
 
+    /*
+    Supportable backup implementation. Creates a backup of the current repository and sets backup date and time
+    of the TechnicalSupport object who called the method.
+    */
     @Override
     public Repository backup(Repository repository) {
         System.out.println("Backing up repository...");
+        /* Call Repository constructor to create new Repository object with the same properties as repository */
         Repository backupRepo = new Repository(repository);
+        /* Set format for string that will store backup repository time and date */
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("uuuu/MM/dd HH:mm:ss");
+        /* Get current time and date */
         LocalDateTime now = LocalDateTime.now();
+        /* Apply format to LocalDateTime object now */
         String backupDateTime = dtf.format(now);
+        /* Set TechnicalSupport lastBackupDateTime to backupDateTime */
         setLastBackupDateTime(backupDateTime);
         System.out.println("New repository backup created on " + backupDateTime + ".\n");
+        /* Return Repository object backupRepo */
         return backupRepo;
     }
 
+    /*
+    Supportable restore implementation. Creates a new Repository object based on another Repository object. Allows the
+    technical support team member to restore a repository from a backup by using backup repository as the method argument.
+    */
     @Override
-    public Repository restore(Repository repository) {
+    public Repository restore(Repository backup) {
+        /* Print backup time and date of backup repository (stored in TechnicalSupport object calling restore) */
         System.out.println("Restoring repository from " + getLastBackupDateTime() + " backup");
-        Repository newRepository = new Repository(repository);
+        /* Call Repository constructor to create new Repository object based on backup */
+        Repository newRepository = new Repository(backup);
         System.out.println("Repository restored from " + getLastBackupDateTime() + ".\n");
+        /* return Repository object newRepository */
         return newRepository;
     }
 
+    /* Supportable clearRepository implementation. Clears the current repository contents. */
     @Override
     public void clearRepository(Repository repository) {
         System.out.println("Clearing current repository...");
+        /* Use setters to set all repository variables to null */
         repository.setOptimalAlignment(null);
         repository.setSNPAlignment(null);
         repository.setUserAlignments(null);
