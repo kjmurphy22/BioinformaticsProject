83099995230da59abff0100c2e15b83c9e92ec0d
kjmurphy22 <99885084+kjmurphy22@users.noreply.github.com>
2023-01-06 19:17:58 +0100

    Main method comments

--- src/MainMethod/MainMethod.java
+++ src/MainMethod/MainMethod.java
@@ -17,20 +17,28 @@ import java.util.HashMap;
 public class MainMethod {
 
     public static void main(String[] args) {
+        /* Create filepaths for the team and fasta files */
         String teamFile = "src/" + args[0];
         String fastaFile = "src/" + args[1];
 
+        /* Call constructors to create a new TeamReadr object and FastaReader object */
         TeamReader teamInput = new TeamReader(teamFile);
         FastaReader fastaInput = new FastaReader(fastaFile);
 
+        /* Call createTeam to create team from the teamFile */
         ArrayList<TeamMember> team = teamInput.createTeam();
 
+        /*
+        Cast subclass type and create references to the
+        TeamMember objects created by the createTeam call above
+        */
         TeamLead teamLead = (TeamLead) team.get(0);
         Bioinformatician bioinformatician1 = (Bioinformatician) team.get(1);
         Bioinformatician bioinformatician2 = (Bioinformatician) team.get(2);
         Bioinformatician bioinformatician3 = (Bioinformatician) team.get(3);
         TechnicalSupport techSupport = (TechnicalSupport) team.get(4);
 
+        /* Set first genome in fastaFile as reference genome */
         String referenceGenome;
         try (BufferedReader br = new BufferedReader(new FileReader(fastaFile))){
             referenceGenome = br.readLine().substring(1);
@@ -41,25 +49,38 @@ public class MainMethod {
             throw new RuntimeException(e);
         }
 
-
+        /* Call readFile to extract genomes from fastaFile */
         HashMap<String, String> genomes = fastaInput.readFile();
+        /* Create new OptimalAlignment object optimal from genomes*/
         OptimalAlignment optimal = new OptimalAlignment(genomes, referenceGenome);
-//        SNPAlignment snp = optimal.createSNPAlignment();
 
+        /* Create a StandardAlignment object for each bioinformatician in the team and assign their name to it */
         StandardAlignment standardAlignment1 = new StandardAlignment(optimal, bioinformatician1.getName());
         StandardAlignment standardAlignment2 = new StandardAlignment(optimal, bioinformatician2.getName());
+        StandardAlignment standardAlignment3 = new StandardAlignment(optimal, bioinformatician3.getName());
 
+        /* Assign each bioinformatician to their respective StandardAlignment object */
         bioinformatician1.setAlignment(standardAlignment1);
         bioinformatician2.setAlignment(standardAlignment2);
+        bioinformatician3.setAlignment(standardAlignment3);
 
+        /* Create a HashMap object to store all bioinformatician's names and their StandardAlignment objects */
         HashMap<String, StandardAlignment> userAlignments = new HashMap<String, StandardAlignment>();
+        /*
+        Set userAlignments object to TeamLead object so TeamLead
+        can access all bioinformatician's alignments.
+        */
         teamLead.setAssignedBioinformaticans(userAlignments);
 
+        /* Add each bioinformatician to TeamLead's list of bioinformaticians and their alignments */
         userAlignments.put(bioinformatician1.getName(), bioinformatician1.getAlignment());
         userAlignments.put(bioinformatician2.getName(), bioinformatician2.getAlignment());
+        userAlignments.put(bioinformatician3.getName(), bioinformatician3.getAlignment());
 
+        /* Create repository based on the initial optimal alignment and assigned bioinformaticians */
         Repository repository = new Repository(optimal, teamLead.getAssignedBioinformaticians());
-
+        /* Create backup repository based on initial repository */
+        Repository backup = techSupport.backup(repository);
 
 //        bioinformatician1.addGenome("test_genomes.fasta");
 
